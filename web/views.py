0b8930b8d1fa1befe60d9142fe8e78e714df3d38
yijingping <jingping.yi@gmail.com>
2015-12-30 01:08:15 +0800

    add jquery,bootstrap

---
+++ web/views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+__author__ = 'yijingping'
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def index(request):
+    return render_to_response('index.html', RequestContext(request, {}))
