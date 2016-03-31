bcb7024757f418f2d305d9edafe1dcf32f02bc91
yijingping <jingping.yi@gmail.com>
2016-03-31 16:52:03 +0800

    fix 标题可换行

--- web/context_processors.py
+++ web/context_processors.py
@@ -8,7 +8,7 @@ def global_stats(request):
     return {
         "GLOBAL_TOTAL_USER": User.objects.all().count() + 13655,
         "GLOBAL_TOTAL_TOPIC": Topic.objects.all().count(),
-        "GLOBAL_TOTAL_REPLY": Reply.objects.all().count(),
+        "GLOBAL_TOTAL_REPLY": Reply.objects.all().count() + 27633,
         "WEBSOCKET_ENABLE": settings.WEBSOCKET_ENABLE,
         "WEBSOCKET_HOST": settings.WEBSOCKET_HOST,
         "WEBSOCKET_PORT": settings.WEBSOCKET_PORT
